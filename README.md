b2d7dd5da95cd53261a59ad30d435664dcc3f915
tom-englert <mail@tom-englert.de>
2023-03-11 14:30:03 +0100

    Add the scaffold for the analyzer to the solution

--- README.md
+++ README.md
@@ -21,3 +21,70 @@ so e.g. a basic user documentation can be generated automatically for dedicated 
 
 > In real life the will be probably a more specific attribute than a simple `[Text]`, this is just used to make this sample more universal.
 
+## Add the scaffold for the analyzer to the solution
+
+In the first step the scaffold for the analyzers and the corresponding tests will be added to the solution.
+
+> Using the "Analyzer with Code Fix" template adds too much unused stuff, with problematic defaults, so it's better to start from scratch with sanitized test verifiers.
+
+### Add an empty project "SolutionAnalyzer":
+```xml
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+    <IsPackable>false</IsPackable>
+  </PropertyGroup>
+  <ItemGroup>
+    <PackageReference Include="Microsoft.CodeAnalysis.CSharp" Version="4.5.0" />
+  </ItemGroup>
+  <Import Project="Packaging.targets" Condition="'$(IsPackable)'=='True'" />
+</Project>
+```
+<sup><a href='/src/SolutionAnalyzer/SolutionAnalyzer/SolutionAnalyzer.csproj' title='Snippet source file'>snippet source</a></sup>
+
+### Add an empty project "SolutionAnalyzer.Test"
+```xml
+<Project Sdk="Microsoft.NET.Sdk">
+  <ItemGroup>
+    <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.5.0" />
+    <PackageReference Include="MSTest.TestAdapter" Version="3.0.2" />
+    <PackageReference Include="MSTest.TestFramework" Version="3.0.2" />
+    <PackageReference Include="Microsoft.CodeAnalysis.CSharp.Analyzer.Testing.MSTest" Version="1.1.1" />
+    <PackageReference Include="Microsoft.CodeAnalysis.CSharp.CodeFix.Testing.MSTest" Version="1.1.1" />
+    <PackageReference Include="Microsoft.CodeAnalysis.CSharp.CodeRefactoring.Testing.MSTest" Version="1.1.1" />
+    <PackageReference Include="Microsoft.CodeAnalysis.CSharp.Workspaces" Version="4.5.0" />
+  </ItemGroup>
+  <ItemGroup>
+    <ProjectReference Include="..\SolutionAnalyzer\SolutionAnalyzer.csproj" />
+  </ItemGroup>
+</Project>
+```
+<sup><a href='/src/SolutionAnalyzer/SolutionAnalyzer.Test/SolutionAnalyzer.Test.csproj' title='Snippet source file'>snippet source</a></sup>
+
+### Add the sanitized test verifiers to the test project
+
+<!-- snippet: CSharpAnalyzerVerifier -->
+<a id='snippet-csharpanalyzerverifier'></a>
+```cs
+internal static class CSharpAnalyzerVerifier<TAnalyzer>
+    where TAnalyzer : DiagnosticAnalyzer, new()
+{
+    public class Test : CSharpAnalyzerTest<TAnalyzer, Verifier>
+    {
+        public Test(string source)
+        {
+            TestCode = source;
+            ReferenceAssemblies = Default.ReferenceAssemblies;
+            TestBehaviors = Default.TestBehaviors;
+        }
+
+        protected override CompilationOptions CreateCompilationOptions() => Default.CompilationOptions;
+
+        protected override ParseOptions CreateParseOptions() => Default.ParseOptions;
+    }
+}
+```
+<sup><a href='/src/SolutionAnalyzer/SolutionAnalyzer.Test/Verifiers.cs#L18-L38' title='Snippet source file'>snippet source</a> | <a href='#snippet-csharpanalyzerverifier' title='Start of snippet'>anchor</a></sup>
+<!-- endSnippet -->
+> see source for the full version including defaults and extension methods
+
---
+++ src/SolutionAnalyzer/SolutionAnalyzer.Test/Verifiers.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+
+using JetBrains.Annotations;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+
+using Verifier = Microsoft.CodeAnalysis.Testing.Verifiers.MSTestVerifier;
+
+namespace SolutionAnalyzer.Test;
+
+// begin-snippet:  CSharpAnalyzerVerifier
+
+internal static class CSharpAnalyzerVerifier<TAnalyzer>
+    where TAnalyzer : DiagnosticAnalyzer, new()
+{
+    public class Test : CSharpAnalyzerTest<TAnalyzer, Verifier>
+    {
+        public Test(string source)
+        {
+            TestCode = source;
+            ReferenceAssemblies = Default.ReferenceAssemblies;
+            TestBehaviors = Default.TestBehaviors;
+        }
+
+        protected override CompilationOptions CreateCompilationOptions() => Default.CompilationOptions;
+
+        protected override ParseOptions CreateParseOptions() => Default.ParseOptions;
+    }
+}
+
+// end-snippet
+
+internal static class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
+    where TAnalyzer : DiagnosticAnalyzer, new()
+    where TCodeFix : CodeFixProvider, new()
+{
+    public class Test : CSharpCodeFixTest<TAnalyzer, TCodeFix, Verifier>
+    {
+        public Test(string source, string? fixedSource = null)
+        {
+            TestCode = source;
+            FixedCode = fixedSource!;
+            ReferenceAssemblies = Default.ReferenceAssemblies;
+            TestBehaviors = Default.TestBehaviors;
+        }
+
+        protected override CompilationOptions CreateCompilationOptions() => Default.CompilationOptions;
+
+        protected override ParseOptions CreateParseOptions() => Default.ParseOptions;
+    }
+}
+
+internal static class CSharpCodeRefactoringVerifier<TCodeRefactoring>
+    where TCodeRefactoring : CodeRefactoringProvider, new()
+{
+    public class Test : CSharpCodeRefactoringTest<TCodeRefactoring, Verifier>
+    {
+        public Test(string source, string? fixedSource = null)
+        {
+            TestCode = source;
+            FixedCode = fixedSource!;
+            ReferenceAssemblies = Default.ReferenceAssemblies;
+            TestBehaviors = Default.TestBehaviors;
+        }
+
+        protected override CompilationOptions CreateCompilationOptions() => Default.CompilationOptions;
+
+        protected override ParseOptions CreateParseOptions() => Default.ParseOptions;
+    }
+}
+
+internal static class Default
+{
+    public static ParseOptions ParseOptions => new CSharpParseOptions(LanguageVersion.CSharp10, DocumentationMode.Diagnose);
+
+    public static ReferenceAssemblies ReferenceAssemblies => ReferenceAssemblies.NetStandard.NetStandard21;
+
+    public static CompilationOptions CompilationOptions
+    {
+        get
+        {
+            var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true);
+
+            return compilationOptions.WithSpecificDiagnosticOptions(compilationOptions.SpecificDiagnosticOptions.SetItems(NullableWarnings));
+        }
+    }
+
+    public static TestBehaviors TestBehaviors => TestBehaviors.SkipGeneratedCodeCheck | TestBehaviors.SkipSuppressionCheck;
+
+    /// <summary>
+    /// By default, the compiler reports diagnostics for nullable reference types at
+    /// <see cref="DiagnosticSeverity.Warning"/>, and the analyzer test framework defaults to only validating
+    /// diagnostics at <see cref="DiagnosticSeverity.Error"/>. This map contains all compiler diagnostic IDs
+    /// related to nullability mapped to <see cref="ReportDiagnostic.Error"/>, which is then used to enable all
+    /// of these warnings for default validation during analyzer and code fix tests.
+    /// </summary>
+    private static ImmutableDictionary<string, ReportDiagnostic> NullableWarnings { get; } = GetNullableWarningsFromCompiler();
+
+    private static ImmutableDictionary<string, ReportDiagnostic> GetNullableWarningsFromCompiler()
+    {
+        string[] args = { "/warnaserror:nullable" };
+        var commandLineArguments = CSharpCommandLineParser.Default.Parse(args, baseDirectory: Environment.CurrentDirectory, sdkDirectory: Environment.CurrentDirectory);
+        var nullableWarnings = commandLineArguments.CompilationOptions.SpecificDiagnosticOptions;
+        return nullableWarnings;
+    }
+}
+
+[PublicAPI]
+internal static class CSharpVerifierExtensionMethods
+{
+    public static TTest AddPackages<TTest>(this TTest test, params PackageIdentity[] packages)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        test.ReferenceAssemblies = test.ReferenceAssemblies.WithPackages(packages.ToImmutableArray());
+        return test;
+    }
+
+    public static TTest AddDiagnostics<TTest>(this TTest test, params DiagnosticResult[] expected)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        test.ExpectedDiagnostics.AddRange(expected);
+        return test;
+    }
+
+    public static TTest AddSolutionTransform<TTest>(this TTest test, Func<Solution, Project, Solution> transform)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        test.SolutionTransforms.Add((solution, projectId) =>
+        {
+            var project = solution.GetProject(projectId);
+            return project == null ? solution : transform(solution, project);
+        });
+
+        return test;
+    }
+
+    public static TTest AddSources<TTest>(this TTest test, params string[] sources)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        foreach (var source in sources)
+        {
+            test.TestState.Sources.Add(source);
+        }
+
+        return test;
+    }
+
+    public static TTest AddReferences<TTest>(this TTest test, params Assembly[] localReferences)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        test.SolutionTransforms.Add((solution, projectId) =>
+        {
+            var localMetadataReferences = localReferences
+                .Distinct()
+                .Select(assembly => MetadataReference.CreateFromFile(assembly.Location));
+
+            solution = solution.AddMetadataReferences(projectId, localMetadataReferences);
+
+            return solution;
+        });
+
+        return test;
+    }
+
+    public static TTest WithReferenceAssemblies<TTest>(this TTest test, ReferenceAssemblies referenceAssemblies)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        if (test.ReferenceAssemblies.Packages.Any())
+            throw new InvalidOperationException("You must call add packages after specifying reference assemblies");
+
+        test.ReferenceAssemblies = referenceAssemblies;
+        return test;
+    }
+
+    public static TTest WithLangVersion<TTest>(this TTest test, LanguageVersion languageVersion)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        test.SolutionTransforms.Add((solution, projectId) => solution.WithProjectParseOptions(projectId, new CSharpParseOptions(languageVersion, DocumentationMode.Diagnose)));
+        return test;
+    }
+
+    public static TTest WithProjectCompilationOptions<TTest>(this TTest test, Func<CompilationOptions, CompilationOptions> callback)
+        where TTest : AnalyzerTest<Verifier>
+    {
+        test.AddSolutionTransform((solution, project) => solution.WithProjectCompilationOptions(project.Id, callback(project.CompilationOptions ?? Default.CompilationOptions)));
+
+        return test;
+    }
+}
